977e1f4b7e03712f2fce8524dff23927c048c0a0
degoel77 <degoel@gmail.com>
2021-08-14 12:51:44 -0700

    Fixed Image Drawing and also added way to save image as a png called NOC Image.

--- gridImage.py
+++ gridImage.py
@@ -1,9 +1,11 @@
+import os
 import sys
-# sys.path.insert(0, '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages')
 
 import pygame
 from pygame.locals import *
-import os
+
+
+# sys.path.insert(0, '/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages')
 
 
 class gridImage:
@@ -16,7 +18,7 @@ class gridImage:
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.init()
         info = pygame.display.Info()
-        self.size = info.current_h
+        self.size = info.current_h - 100
         self.windowSurface = pygame.display.set_mode((self.size, self.size))
         self.windowSurface.fill([0, 0, 0])
         self.WHITE = (255, 255, 255)
@@ -24,15 +26,15 @@ class gridImage:
         self.GREEN = (0, 255, 0)
         self.BLUE = (0, 0, 255)
         self.PURPLE = (255, 0, 255)
-        self.margin = 100
-        self.cellWidthX = (self.size - 2 * self.margin)/self.GRID_SIZE_X
-        self.cellWidthY = (self.size - 2 * self.margin)/self.GRID_SIZE_Y
-        self.circleRadius = (self.cellWidthX + self.cellWidthY)/12
+        self.margin = 25
+        self.cellWidthX = (self.size - 2 * self.margin) / self.GRID_SIZE_X
+        self.cellWidthY = (self.size - 2 * self.margin) / self.GRID_SIZE_Y
+        self.circleRadius = (self.cellWidthX + self.cellWidthY) / 10
         self.lineWidth = 4
         print(f"SELF.CELLWIDTHX = {self.cellWidthY}")
         self.X = 100
         self.Y = 100
-        self.basicFont = pygame.font.SysFont(None, 18)
+        self.basicFont = pygame.font.SysFont(None, int(self.size / 50))
         # draw circles
         for i in range(self.GRID_SIZE_X):
             for j in range(self.GRID_SIZE_Y):
@@ -111,6 +113,7 @@ class gridImage:
 
     def run(self):
         pygame.display.update()
+        pygame.image.save(self.windowSurface, "NOCImage.png")
         # run the game loop
         while True:
             for event in pygame.event.get():
